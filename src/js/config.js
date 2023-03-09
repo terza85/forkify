05f0fb8227b8d191cc4479b5e53d518405254e75
terza85 <terzicmarko@gmail.com>
2023-03-09 20:13:25 +0100

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7fa8628b-eb12-43ef-8cdd-12be1a5a862b';
+export const MODAL_CLOSE_SEC = 2.5;
